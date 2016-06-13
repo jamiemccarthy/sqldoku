ba6522c6138ff16cd65a26e5f2c424c0a1cd55dc
Jamie McCarthy <jamie@mccarthy.vg>
2016-06-13 09:02:55 -0400

    puzzles schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160613125707) do
+
+  create_table "puzzles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "uuid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean  "solved"
+    t.index ["uuid"], name: "index_puzzles_on_uuid", unique: true, using: :btree
+  end
 
 end
