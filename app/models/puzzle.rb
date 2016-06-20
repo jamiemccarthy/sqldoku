e0219b193892d89cd3fb3bd5ae21fa3e12f7b966
Jamie McCarthy <jamie@mccarthy.vg>
2016-06-19 21:05:32 -0400

    Puzzle.build_cells

--- app/models/puzzle.rb
+++ app/models/puzzle.rb
@@ -10,7 +10,21 @@ class Puzzle < ApplicationRecord
   # root**6 total possibilities.
   validates :root, :numericality => { :greater_than_or_equal_to => 2, :less_than_or_equal_to => 10 }
 
+  def build_cells
+    (1..root**2).each do |row|
+      (1..root**2).each do |col|
+        (1..root**2).each do |symbol|
+          cells.build(:symbol => symbol, :col => col, :row => row, :blk => calculate_blk(col, row))
+        end
+      end
+    end
+  end
+
   protected
+
+  def calculate_blk(col, row)
+    1 + (((row-1)/root)*root + (col-1)/root)
+  end
 
   def ensure_uuid
     self.uuid ||= SecureRandom.uuid
