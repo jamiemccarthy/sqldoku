df7815ff64e5e0d597623fccb367127ab45fe324
Jamie McCarthy <jamie@mccarthy.vg>
2016-06-25 10:44:20 -0400

    Cell scopes+updates, Puzzle set!

--- app/models/cell.rb
+++ app/models/cell.rb
@@ -1,3 +1,21 @@
 class Cell < ApplicationRecord
-  belongs_to :puzzle
+  belongs_to :puzzle, :touch => true
+
+  scope :with_symbol, ->(symbol) { where(:symbol => symbol) }
+  scope :in_col, ->(c) { where(:col => c) }
+  scope :in_row, ->(r) { where(:row => r) }
+  scope :in_blk, ->(b) { where(:blk => b) }
+  scope :is_possible, -> { where(:possible => 1) }
+  scope :is_confirmed, -> { where(:confirmed => 1) }
+  scope :natural_order, -> { order(:col => :asc, :row => :asc, :symbol => :asc) }
+
+  def impossible!
+    raise ArgumentError if confirmed
+    update(:possible => 0)
+  end
+
+  def confirmed!
+    raise ArgumentError unless possible
+    update(:confirmed => 1)
+  end
 end
--- app/models/puzzle.rb
+++ app/models/puzzle.rb
@@ -18,15 +18,47 @@ class Puzzle < ApplicationRecord
         end
       end
     end
+    cells.size
+  end
+
+  def set!(col, row, symbol)
+    blk = calculate_blk(col, row)
+    Puzzle.transaction do
+      surrounding_scopes(col, row, symbol).each { |skope| bulk_impossible!(skope) }
+      cells.with_symbol(symbol).in_col(col).in_row(row).each { |c| c.confirmed! }
+    end
+  end
+
+  # This method encapsulates the logic at the heart of sudoku, which is that
+  # defining a symbol locks down other symbols in four ways:
+  #   * no other symbol is possible in that cell;
+  #   * that same symbol cannot repeat in the same row, column, nor block.
+  def surrounding_scopes(col, row, symbol)
+    blk = calculate_blk(col, row)
+    [
+      cells.in_row(row).in_col(col).where.not(:symbol => symbol),
+      cells.with_symbol(symbol).in_row(row).where.not(:col => col),
+      cells.with_symbol(symbol).in_col(col).where.not(:row => row),
+      cells.with_symbol(symbol).in_blk(blk).where.not(:col => col, :row => row)
+    ]
   end
 
   protected
+
+  def ensure_uuid
+    self.uuid ||= SecureRandom.uuid
+  end
 
   def calculate_blk(col, row)
     1 + (((row-1)/root)*root + (col-1)/root)
   end
 
-  def ensure_uuid
-    self.uuid ||= SecureRandom.uuid
+  def bulk_impossible!(skope)
+    # We're bulk-setting these cells to "impossible," which means none of them
+    # can be "confirmed." As an alternative, we could individually invoke
+    # ".each { |cell| cell.impossible! }", which would raise the same error,
+    # but this is slower than checking them all at once.
+    raise ArgumentError unless skope.is_confirmed.count == 0
+    skope.update_all(:possible => 0)
   end
 end
